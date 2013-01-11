bec9f1debc7705985f92c73f3e5500a4028ad46c
Taha Hachana <tahahachana@live.com>
2013-01-11 13:03:30 +0100

    first commit

---
+++ XTract/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+﻿namespace XTract
+
+open System.Reflection
+
+module AssemblyInfo =
+
+    [<
+        AssemblyCopyright("Copyright Taha Hachana 2013")       ;
+        AssemblyDescription("HTML Data Extraction Component.") ;
+        AssemblyFileVersion("0.1.0.0")                         ;
+        AssemblyProduct("XTract")                              ;
+        AssemblyTitle("XTract")                                ;
+        AssemblyVersion("0.1.6.3")
+    >]
+
+    do ()
---
+++ XTract/String.fs
@@ -0,0 +1,16 @@
+﻿namespace XTract
+
+open System.Text.RegularExpressions
+
+module String =
+    
+    let private compileRegex pattern = Regex(pattern, RegexOptions.Compiled)
+
+    /// <summary>Removes line breaks and white space of 2 or more characters from a string.</summary>
+    /// <param name="str">The string to process.</param>
+    /// <returns>A new string stripped from extra white space.</returns>
+    let stripSpaces str =
+        let regex = compileRegex "(\n|\r)"
+        let regex' = compileRegex " {2,}"
+        regex.Replace(str, " ")
+        |> fun x -> regex'.Replace(x, " ")

02898b8f83e03ae02fddec56ad37e492afa16a6a
Taha Hachana <tahahachana@gmail.com>
2015-01-02 09:48:58 +0100

    Update usage sample

--- Src/AssemblyInfo.fs
+++ Src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 
 [<
     AssemblyDescription("F# screen scraping package.");
-    AssemblyFileVersion("0.1.1");
+    AssemblyFileVersion("0.2.3");
     AssemblyProduct("XTract");
     AssemblyTitle("XTract");
-    AssemblyVersion("0.1.1")
+    AssemblyVersion("0.2.3")
 >]
 
 do ()

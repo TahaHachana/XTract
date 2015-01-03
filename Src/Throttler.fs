8dcc5f1574d5f078c7c3fc7ebfbda0f7585c7fb6
Taha Hachana <tahahachana@gmail.com>
2015-01-03 08:58:02 +0100

    Throttling helpers
    Added a simple function for throttling async computations, it can be
    used to speed up scraping a list of urls by sending n concurrent
    requests.

---
+++ Src/Throttler.fs
@@ -0,0 +1,43 @@
+﻿module XTract.Throttler
+
+open System
+open System.Collections.Generic
+
+type Message = Quit | Work
+
+let throttle asyncs limit callback =
+ 
+    let q = Queue()
+    let dequeue() = try q.Dequeue() |> Some with _ -> None
+    asyncs |> Seq.iter (fun x -> q.Enqueue x)
+    
+    let agent =
+        MailboxProcessor.Start(fun x ->
+            let rec loop count =
+                async {
+                    let! msg = x.Receive()
+                    match msg with
+                    | Work ->
+                        let work = dequeue()
+                        match work with
+                        | Some work' ->
+                            async {
+                                try
+                                    do! work'
+                                finally
+                                    x.Post Work
+                            } |> Async.Start
+                            return! loop count
+                        | None ->
+                            x.Post Quit
+                            return! loop (count + 1)
+                    | Quit ->
+                        match count with
+                        | _ when count = limit ->
+                            callback |> Async.Start
+                            (x:> IDisposable).Dispose()
+                        | _ -> return! loop count
+                }
+            loop 0
+        )
+    [1 .. limit] |> List.iter (fun _ -> agent.Post Work)

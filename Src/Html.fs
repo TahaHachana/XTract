a0fbfdfb967272959f07cb3259df6e41b8f77fc0
Taha Hachana <tahahachana@gmail.com>
2015-02-15 17:54:50 +0100

    Add links module

--- Src/Html.fs
+++ Src/Html.fs
@@ -163,105 +163,105 @@ let headings html =
         )
         |> Some
 
-type Hyperlink =
-    {
-        Href: string
-        Anchor: string
-        Type: LinkType
-        Rel: Rel
-    }
-
-    static member New href anchor linkType rel =
-        {
-            Href = href
-            Anchor = anchor
-            Type = linkType
-            Rel = rel
-        }
-
-and LinkType = External | Internal
-
-and Rel = Follow | NoFollow
-
-let internal baseUri root url =
-    xpathSelect root "//base"
-    |> function
-    | None -> url
-    | Some htmlNode ->
-        htmlNode.GetAttributeValue("href", "")
-        |> function
-        | "" -> url
-        | x -> x
-    |> fun x ->
-        Uri.TryCreate(x, UriKind.Absolute)
-        |> function
-        | false, _ -> None
-        | true, uri -> Some uri
-
-let internal makeLink host (href:string) (rel:string) anchor =
-    let linkType =
-        match href.Contains host with
-        | false -> External
-        | true -> Internal
-    let rel' =
-        match rel.ToLowerInvariant().Contains("nofollow") with
-        | false -> Follow
-        | true -> NoFollow
-    Hyperlink.New href anchor linkType rel'
-
-let internal aTags root =
-    xpathSelectAll root "//a"
-
-let internal linkAttrs (htmlNode:HtmlNode) =
-    let href = htmlNode.GetAttributeValue("href", "")
-    let rel = htmlNode.GetAttributeValue("rel", "")
-    let anchor =
-        htmlNode.InnerText
-        |> String.stripHtml
-        |> String.decodeHtml
-        |> String.stripSpaces
-    href, rel, anchor
-
-let private makeAbsolute (baseUri:Uri) (href:string) rel anchor =
-    Uri.TryCreate(baseUri, href)
-    |> function
-    | false, _ -> None
-    | true, uri -> Some (uri.ToString(), rel, anchor)
-
-let private checkAbsolute href rel anchor =
-    Uri.TryCreate(href, UriKind.Absolute)
-    |> function
-    | false, _ -> None
-    | true, uri -> Some (uri.ToString(), rel, anchor)
-
-let links html url =
-    let root = load html
-    aTags root
-    |> function
-    | None -> None
-    | Some htmlNodes ->
-        let links =
-            htmlNodes
-            |> List.map linkAttrs
-            |> List.filter (fun (href, _, _) ->
-                not <| Regex("(?i)^(mailto:|location\.|javascript:)").IsMatch href)
-        let absolute, relative =
-            links
-            |> List.partition (fun (href, _, _) -> Regex("^http").IsMatch href)
-        let absolute'= absolute |> List.choose (fun (href, rel, anchor) -> checkAbsolute href rel anchor)
-        let ``base`` = baseUri root url
-        let relative' =
-            match ``base`` with
-            | None -> relative
-            | Some baseUri ->
-                relative
-                |> List.choose (fun (href, rel, anchor) ->
-                    makeAbsolute baseUri href rel anchor)
-        Uri.TryCreate(url, UriKind.Absolute)
-        |> function
-        | false, _ -> None
-        | true, uri ->
-            let host = uri.Host
-            List.append relative' absolute'
-            |> List.map (fun (href, rel, anchor) -> makeLink host href rel anchor)
-            |> Some
+//type Hyperlink =
+//    {
+//        Href: string
+//        Anchor: string
+//        Type: LinkType
+//        Rel: Rel
+//    }
+//
+//    static member New href anchor linkType rel =
+//        {
+//            Href = href
+//            Anchor = anchor
+//            Type = linkType
+//            Rel = rel
+//        }
+//
+//and LinkType = External | Internal
+//
+//and Rel = Follow | NoFollow
+//
+//let internal baseUri root url =
+//    xpathSelect root "//base"
+//    |> function
+//    | None -> url
+//    | Some htmlNode ->
+//        htmlNode.GetAttributeValue("href", "")
+//        |> function
+//        | "" -> url
+//        | x -> x
+//    |> fun x ->
+//        Uri.TryCreate(x, UriKind.Absolute)
+//        |> function
+//        | false, _ -> None
+//        | true, uri -> Some uri
+//
+//let internal makeLink host (href:string) (rel:string) anchor =
+//    let linkType =
+//        match href.Contains host with
+//        | false -> External
+//        | true -> Internal
+//    let rel' =
+//        match rel.ToLowerInvariant().Contains("nofollow") with
+//        | false -> Follow
+//        | true -> NoFollow
+//    Hyperlink.New href anchor linkType rel'
+//
+//let internal aTags root =
+//    xpathSelectAll root "//a"
+//
+//let internal linkAttrs (htmlNode:HtmlNode) =
+//    let href = htmlNode.GetAttributeValue("href", "")
+//    let rel = htmlNode.GetAttributeValue("rel", "")
+//    let anchor =
+//        htmlNode.InnerText
+//        |> String.stripHtml
+//        |> String.decodeHtml
+//        |> String.stripSpaces
+//    href, rel, anchor
+//
+//let private makeAbsolute (baseUri:Uri) (href:string) rel anchor =
+//    Uri.TryCreate(baseUri, href)
+//    |> function
+//    | false, _ -> None
+//    | true, uri -> Some (uri.ToString(), rel, anchor)
+//
+//let private checkAbsolute href rel anchor =
+//    Uri.TryCreate(href, UriKind.Absolute)
+//    |> function
+//    | false, _ -> None
+//    | true, uri -> Some (uri.ToString(), rel, anchor)
+//
+//let links html url =
+//    let root = load html
+//    aTags root
+//    |> function
+//    | None -> None
+//    | Some htmlNodes ->
+//        let links =
+//            htmlNodes
+//            |> List.map linkAttrs
+//            |> List.filter (fun (href, _, _) ->
+//                not <| Regex("(?i)^(mailto:|location\.|javascript:)").IsMatch href)
+//        let absolute, relative =
+//            links
+//            |> List.partition (fun (href, _, _) -> Regex("^http").IsMatch href)
+//        let absolute'= absolute |> List.choose (fun (href, rel, anchor) -> checkAbsolute href rel anchor)
+//        let ``base`` = baseUri root url
+//        let relative' =
+//            match ``base`` with
+//            | None -> relative
+//            | Some baseUri ->
+//                relative
+//                |> List.choose (fun (href, rel, anchor) ->
+//                    makeAbsolute baseUri href rel anchor)
+//        Uri.TryCreate(url, UriKind.Absolute)
+//        |> function
+//        | false, _ -> None
+//        | true, uri ->
+//            let host = uri.Host
+//            List.append relative' absolute'
+//            |> List.map (fun (href, rel, anchor) -> makeLink host href rel anchor)
+//            |> Some
---
+++ Src/Links.fs
@@ -0,0 +1,131 @@
+﻿module XTract.Links
+
+open System
+open HtmlAgilityPack
+open System.Text.RegularExpressions
+
+/// Describes a link within a HTML document.
+type Hyperlink =
+    {
+        uri: Uri
+        anchor: string
+        destination: Destination
+        nofollow: bool
+        referrer: string
+    }
+
+    static member New uri anchor destination nofollow referrer =
+        {
+            uri= uri
+            anchor = anchor
+            destination = destination
+            nofollow = nofollow
+            referrer = referrer
+        }
+
+and Destination = External | Internal
+
+module private Helpers =
+
+    /// Gets the root node of a HTML document.
+    let rootNode html=
+        let document = HtmlDocument()
+        document.LoadHtml html
+        document.DocumentNode
+
+    /// Selects the nodes matching the "//a" XPath expression
+    /// from the root node.
+    let selectLinks (root:HtmlNode) =
+        root.SelectNodes "//a"
+        |> function
+        | null -> None
+        | htmlNodes ->
+            Seq.toList htmlNodes
+            |> Some
+
+    let tryCreaTeUri uriString =
+        match Uri.TryCreate(uriString, UriKind.Absolute) with
+        | false, _ -> None
+        | true, uri -> Some uri
+
+    /// Returns the base URL for all relative URLs in a document.
+    /// If the document doesn't specify one then the request
+    /// URL is used.
+    let getBaseUri (root:HtmlNode) requestUri =
+        root.SelectSingleNode "//base"
+        |> function
+        | null -> tryCreaTeUri requestUri
+        | htmlNode ->
+            match htmlNode.GetAttributeValue("href", "") with
+            | "" -> tryCreaTeUri requestUri
+            | href ->
+                match tryCreaTeUri href with
+                | None -> tryCreaTeUri requestUri
+                | Some uri -> Some uri
+
+    /// Returns the destination address of a link as a URI.
+    let hyperlinkUri (linkNode: HtmlNode) (baseUri: Uri option) =
+        let href = linkNode.GetAttributeValue("href", "")
+        match Regex("(?i)^(mailto:|location\.|javascript:)").IsMatch href with
+        | false ->
+            let isAbsolute = href.StartsWith "http"
+            match isAbsolute with
+            | false ->
+                match baseUri with
+                | None -> None
+                | Some uri ->
+                    match Uri.TryCreate(uri, href) with
+                    | false, _ -> None
+                    | true, uri -> Some uri
+            | true -> tryCreaTeUri href
+        | true -> None
+
+    /// Returns the anchor of a link.
+    let getAnchor (linkNode:HtmlNode) =
+        linkNode.InnerText
+        |> String.stripHtml
+        |> String.decodeHtml
+        |> String.stripSpaces
+
+    /// Determines the destination of a link's address.
+    let getDestination (uri:Uri) host =
+        match uri.Host = host with
+        | false-> External
+        | _ -> Internal
+
+    /// Checks if the link's rel attribute has the "nofollow" value.
+    let isNofollow (linkNode: HtmlNode) =
+        let rel = linkNode.GetAttributeValue("rel", "")
+        rel.ToLower().Contains("nofollow")
+
+    let makeHyperlink baseUri host requestUri linkNode =
+        let uriOption = hyperlinkUri linkNode baseUri
+        match uriOption with
+        | None -> None
+        | Some uri ->
+            let anchor = getAnchor linkNode
+            let destination = getDestination uri host
+            let nofollow = isNofollow linkNode
+            Hyperlink.New uri anchor destination nofollow requestUri
+            |> Some
+
+open Helpers
+
+/// Collects a hyperlinks list from the specified HTML document.
+/// The referrer field is set to the specified request URL.
+let fromHtml html requestUri =
+    let root = rootNode html
+    let links = selectLinks root
+    match links with
+    | None -> None
+    | _ ->
+        let baseUri = getBaseUri root requestUri
+        match baseUri with
+        | None -> None
+        | _ ->
+            let host = (Option.get baseUri).Host
+            Option.get links
+            |> List.choose (makeHyperlink baseUri host requestUri)
+            |> function
+            | [] -> None
+            | lst -> Some lst

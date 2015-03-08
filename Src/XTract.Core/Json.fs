6c01cb412de7e139ef3a69ef96efe9c5560408e5
Taha Hachana <tahahachana@gmail.com>
2015-03-08 14:20:45 +0100

    More JSON helpers
    * serializeWrite: Serializes the specified object to a JSON string and
    writes it to the supplied file.
    * readRecords<'T>: Reads a JSON string from the specified file and
    deserializes it to the supplied F# record type.

--- Src/XTract.Core/Json.fs
+++ Src/XTract.Core/Json.fs
@@ -2,6 +2,7 @@
 module XTract.Json
 
 open Newtonsoft.Json
+open System.IO
 
 let toRecords<'T> json =
     JsonConvert.DeserializeObject(json, typeof<seq<'T>>)
@@ -14,4 +15,19 @@ let decodeHtml (json:string) =
     json.Replace("""\""", "")
         .Replace("u003C", "<")
         .Replace("u003E", ">")
+
+/// <summary>Serializes the specified object to a JSON string and
+/// writes it to the supplied file.</summary>
+/// <param name="path">The file to write to.</param>
+/// <param name="value">The object to serialize.</param>
+let serializeWrite path value =
+    serialize value
+    |> fun json -> File.WriteAllText(path, json)
+
+/// <summary>Reads a JSON string from the specified file and
+/// deserializes it to the supplied F# record type.</summary>
+/// <param name="path">The file to read from.</param>
+let readRecords<'T> path =
+    File.ReadAllText path
+    |> toRecords<'T>
 

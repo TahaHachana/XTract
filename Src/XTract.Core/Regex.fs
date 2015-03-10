032048298d476d91347ae2c9e96ac012fca41049
Taha Hachana <tahahachana@gmail.com>
2015-03-10 14:06:20 +0100

    Clean and comment the regex module

--- Src/XTract.Core/Regex.fs
+++ Src/XTract.Core/Regex.fs
@@ -31,92 +31,48 @@ let patternMatch pattern input =
     compile pattern
     |> fun regex -> ``match`` input regex 
 
-
-
-
-
-/// <summary>Returns the regex matches found in the input string using the specified pattern.</summary>
+/// <summary>Compiles a new regular expression for the specified pattern
+/// and searches the input string for all its occurrences.</summary>
 /// <param name="pattern">The regular expression pattern.</param>
-/// <param name="inputString">The input string.</param>
-/// <returns>The match list.</returns>
+/// <param name="input">The input string.</param>
 let patternMatches pattern input =
     compile pattern
     |> fun x -> matches input x
 
-/// <summary>Returns the value of a group in a regex match.</summary>
-/// <param name="matchObj">The regex match object.</param>
-/// <param name="group">The group index.</param>
-/// <returns>The specified group value.</returns>    
-let matchGroup (matchObj : Match) (group : int) = matchObj.Groups.[group].Value
+/// <summary>Gets the value of the nth group matched
+/// matched by the regular expression.</summary>
+/// <param name="match">The regular expression match.</param>
+/// <param name="index">The group index.</param>
+let nthGroupValue (``match``: Match) (index: int) = ``match``.Groups.[index].Value
 
-/// <summary>Returns the first regex match found in the specified input string.</summary>
-/// <param name="regex">The Regex object.</param>
-/// <param name="inputString">The input string.</param>
-/// <returns>The first match object.</returns>    
-let getMatch (regex : Regex) inputString = regex.Match inputString
+/// <summary>Indicates whether the specified regular expression pattern
+/// finds a match in the supplied input string.</summary>
+/// <param name="pattern">The regular expression pattern.</param>
+/// <param name="input">The input string.</param>
+let isMatch pattern input = Regex(pattern).IsMatch input
 
-/// <summary>Returns the value of a group after matching the specified regex with the input string.</summary>
-/// <param name="regex">The regex object.</param>
-/// <param name="group">The group index.</param>
-/// <param name="inputString">The input string.</param>
-/// <returns>The specified group value.</returns>    
-let regexGroup regex group inputString =
-    getMatch regex inputString
-    |> fun x -> matchGroup x group
-
-/// <summary>Returns the values of a group after matching the specified regex with the input string.</summary>
-/// <param name="regex">The regex object.</param>
-/// <param name="group">The group index.</param>
-/// <param name="inputString">The input string.</param>
-/// <returns>The specified group values.</returns>    
-let regexGroups regex group input =
-    matches input regex
-    |> List.map (fun x -> matchGroup x group)
-
-/// <summary>Returns the value of a group after compiling the specified pattern into a regex and matching it with the input string.</summary>
-/// <param name="regex">The regex object.</param>
-/// <param name="group">The group index.</param>
-/// <param name="inputString">The input string.</param>
-/// <returns>The specified group value.</returns>
-let patternGroup pattern group inputString =
-    Regex pattern
-    |> fun x -> regexGroup x group inputString
-
-/// <summary>Returns the values of a group after compiling the specified pattern into a regex and matching it with the input string.</summary>
-/// <param name="regex">The regex object.</param>
-/// <param name="group">The group index.</param>
-/// <param name="inputString">The input string.</param>
-/// <returns>The specified group values.</returns>
-let patternGroups pattern group inputString =
-    Regex pattern
-    |> fun x -> regexGroups x group inputString
-
-/// <summary>Checks whether a pattern is matched in the specified input string.</summary>
-/// <param name="pattern">The regular expression pattern object.</param>
-/// <param name="inputString">The input string.</param>
-let isMatch pattern inputString = Regex(pattern).IsMatch inputString
-
-/// <summary>Removes substrings that match a pattern from the specified input string.</summary>
-/// <param name="pattern">The regular expression pattern object.</param>
-/// <param name="inputString">The input string.</param>
-let remove pattern inputString =
+/// <summary>Removes all strings that match a regular expression
+/// pattern within the specified input string.</summary>
+/// <param name="pattern">The regular expression pattern.</param>
+/// <param name="input">The input string.</param>
+let remove pattern input =
     let regex = compile pattern
-    regex.Replace(inputString, "")
+    regex.Replace(input, "")
 
 module CommonPatterns =
         
-    let Name     = "[a-zA-Z''-'\s]{1,40}"
-    let SSN      = "\d{3}-\d{2}-\d{4}"
-    let Phone    = "[01]?[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{3}[- .]?\d{4}"
-    let Email    = "(?(\")(\".+?\"@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))"
-    let URI      = "(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?"
-    let ZIP      = "(\d{5}-\d{4}|\d{5}|\d{9})$|^([a-zA-Z]\d[a-zA-Z] \d[a-zA-Z]\d)"
-    let Currency = "(-)?\d+(\.\d\d)?"
+    let name = "[a-zA-Z''-'\s]{1,40}"
+    let ssn = "\d{3}-\d{2}-\d{4}"
+    let phone = "[01]?[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{3}[- .]?\d{4}"
+    let email = "(?(\")(\".+?\"@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))"
+    let uri = "(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?"
+    let zip = "(\d{5}-\d{4}|\d{5}|\d{9})$|^([a-zA-Z]\d[a-zA-Z] \d[a-zA-Z]\d)"
+    let currency = "(-)?\d+(\.\d\d)?"
 
-    let NameRegex     = compile Name
-    let SSNRegex      = compile SSN
-    let PhoneRegex    = compile Phone
-    let EmailRegex    = compile Email
-    let URIRegex      = compile URI
-    let ZIPRegex      = compile ZIP
-    let CurrencyRegex = compile Currency
+    let nameRegex = compile name
+    let ssnRegex = compile ssn
+    let phoneRegex = compile phone
+    let emailRegex = compile email
+    let uriRegex = compile uri
+    let zipRegex = compile zip
+    let currencyRegex = compile currency

4b14180a6c63e08163a40a32604e3bcf3139614a
Taha Hachana <tahahachana@gmail.com>
2015-03-13 14:19:36 +0100

    Clean and comment the string module

--- Src/XTract.Core/String.fs
+++ Src/XTract.Core/String.fs
@@ -1,4 +1,4 @@
-﻿namespace XTract
+﻿module XTract.String
 
 open System
 open System.IO
@@ -6,64 +6,92 @@ open System.Net
 open System.Text.RegularExpressions
 open System.Globalization
 
-module String =
-    
-    /// <summary>Removes line breaks and white space exceeding one character from a string.</summary>
-    /// <param name="str">The string to process.</param>
-    /// <returns>A new string stripped from extra white space.</returns>
-    let stripSpaces str =
-        let regex = Regex.compile "(\n|\r)"
-        let regex' = Regex.compile @"\s+"
-        regex.Replace(str, " ")
-        |> fun x -> regex'.Replace(x, " ").Trim()
+/// <summary>Removes line breaks and extra white space
+/// from the supplied string.</summary>
+/// <param name="input">The input string.</param>
+let stripSpaces input =
+    let lineBreakRegex = Regex.compile "(\n|\r)"
+    let whiteSpaceRegex = Regex.compile @"\s+"
+    lineBreakRegex.Replace(input, " ")
+    |> fun x -> whiteSpaceRegex.Replace(x, " ")
+    |> fun x -> x.Trim()
 
-    let stripInlineJsCss inputString = Regex.remove "(?is)(<script.*?</script>|<style.*?</style>)" inputString
+/// <summary>
+/// Removes inline JavaScript and CSS from the specified
+/// HTML string.
+/// </summary>
+/// <param name="input">The HTML string.</param>
+let stripInlineJsCss input = Regex.remove "(?is)(<script.*?</script>|<style.*?</style>)" input
 
-    let stripTags inputString = Regex.remove "(?s)<.+?>" inputString
+/// <summary>
+/// Removes tags from the supplied HTML string.
+/// </summary>
+/// <param name="input">The HTML string.</param>
+let stripTags input = Regex.remove "(?s)<.+?>" input
 
-    let decodeHtml str = WebUtility.HtmlDecode str
+/// <summary>
+/// Decodes a string that has been HTML-encoded for
+/// HTTP transmission.
+/// </summary>
+/// <param name="input">The HTML-encoded string.</param>
+let htmlDecode input = WebUtility.HtmlDecode input
 
-    let stripHtml str =    
-        stripSpaces str
-        |> stripInlineJsCss
-        |> stripTags
-        |> decodeHtml
-        |> fun x -> x.Trim()
+/// <summary>
+/// Converts the specified input string to an HTML-encoded one.
+/// </summary>
+/// <param name="input">The input string.</param>
+let htmlEncode input = WebUtility.HtmlEncode input
 
-    let removePunctuation inputString = Regex.remove "\\p{P}+" inputString
+/// <summary>
+/// Removes JavaScript, CSS and tags from the supplied
+/// HTML string and decodes HTML-encoded characters.
+/// </summary>
+/// <param name="input">The HTML string.</param>
+let stripHtml input =    
+    stripSpaces input
+    |> stripInlineJsCss
+    |> stripTags
+    |> htmlDecode
+    |> fun x -> x.Trim()
 
-    let checkEmpty =
-        function
-            | ""  -> None
-            | str ->
-                stripSpaces str
-                |> stripInlineJsCss
-                |> decodeHtml
-                |> fun x -> x.Trim()
-                |> function
-                    | ""  -> None
-                    | str -> Some str
+/// <summary>
+/// Removes punctuation from the supplied string.
+/// </summary>
+/// <param name="input">The input string.</param>
+let stripPunctuation input = Regex.remove "\\p{P}+" input
 
-    let validPath path =
-        let regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars())
-        let regex = new Regex(String.Format("[{0}]", Regex.Escape(regexSearch)))
-        regex.Replace(path, "-")
+/// <summary>
+/// Replaces characters that are not allowed in file
+/// and path names within the specified string.
+/// </summary>
+/// <param name="input">The input string.</param>
+/// <param name="replacement">The replacement string.</param>
+let validPath input (replacement: string) =
+    let pattern =
+        [|
+            yield! Path.GetInvalidFileNameChars()
+            yield! Path.GetInvalidPathChars()
+        |]
+        |> fun x -> new string(x)
+        |> fun x -> String.Format("[{0}]", Regex.Escape(x))
+    let regex = Regex.compile pattern
+    regex.Replace(input, replacement)
 
-    /// <summary>Converts the specified string to titlecase.</summary>
-    /// <param name="str">the input string.</param>
-    let toTitleCase (str:string) = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower())
+/// <summary>
+/// Converts the specified string to titlecase.
+/// </summary>
+/// <param name="str">the input string.</param>
+let toTitleCase (str:string) = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower())
 
-    /// <summary>Returns a copy of the input string with the first
-    /// letter converted to uppercase.</summary>
-    /// <param name="str">the input string.</param>
-    let firstCharToUpper (str:string) =
-        match str.Length with
-        | 0 -> ""
-        | 1 -> str.ToUpper()
-        | _ -> 
-            Char.ToUpper str.[0]
-            |> fun x -> string x + str.Substring 1
-
-
-
-
+/// <summary>
+/// Returns a copy of the input string with the first
+/// letter converted to uppercase.
+/// </summary>
+/// <param name="str">the input string.</param>
+let firstCharToUpper (str:string) =
+    match str.Length with
+    | 0 -> ""
+    | 1 -> str.ToUpper()
+    | _ -> 
+        Char.ToUpper str.[0]
+        |> fun x -> string x + str.Substring 1

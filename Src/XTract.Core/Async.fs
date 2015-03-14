6b8eeb5691ab31a0cfe93a01aae4047b94af904b
Taha Hachana <tahahachana@gmail.com>
2015-03-14 19:11:54 +0100

    Async.StartWithTimeout as extension member
    The task's status is now checked recursively to determine whether it ran
    to completetion or was canceled.

--- Src/XTract.Core/Async.fs
+++ Src/XTract.Core/Async.fs
@@ -2,24 +2,43 @@
 module XTract.Async
 
 open System.Threading
-open System.Timers
 open System.Threading.Tasks
+open System.Timers
 
-/// Executes a computation in the thread pool. The computation
-/// is cancelled if it doesn't complete within the specified
-/// interval of milliseconds.
-let StartWithTimeout interval computation =
-    async {
-        try
-            use cancellationSource = new CancellationTokenSource()
-            let cancellationToken = cancellationSource.Token
-            use timer = new Timer(interval)
-            timer.Elapsed.Add(fun _ -> cancellationSource.Cancel())
-            timer.Start()
-            let task = Async.StartAsTask(computation, cancellationToken = cancellationToken)
-            task.Wait()
-            match task.Status with
-            | TaskStatus.RanToCompletion -> return task.Result
-            | _ -> return None
-        with _ -> return None
-    }
+/// Checks the status of the supplied task recursively.
+/// Returns the task's result if it ran to completion and
+/// None if it faulted or was canceled.
+let rec private checkStatus (task: Task<'T option>) =
+    match task.Status with
+    | TaskStatus.Created
+    | TaskStatus.WaitingForActivation
+    | TaskStatus.WaitingToRun
+    | TaskStatus.Running -> checkStatus task
+    | TaskStatus.RanToCompletion -> task.Result
+    | _ -> None
+
+type Async with
+
+    /// <summary>
+    /// Executes a computation in the thread pool. The computation
+    /// is canceled if it doesn't complete within the specified
+    /// milliseconds interval.
+    /// </summary>
+    /// <param name="millisecondsDueTimeout">The timeout interval.</param>
+    /// <param name="computation">The asynchronous computation.</param>
+    static member StartWithTimeout millisecondsDueTimeout computation =
+        async {
+            try
+                use cancellationSource = new CancellationTokenSource()
+                let cancellationToken = cancellationSource.Token
+
+                use timer = new Timer(float millisecondsDueTimeout)         
+                timer.Elapsed.Add(fun _ -> cancellationSource.Cancel())
+                timer.Start()
+
+                let task = Async.StartAsTask(computation, cancellationToken = cancellationToken)
+                
+                let result = checkStatus task
+                return result
+            with _ -> return None
+        }

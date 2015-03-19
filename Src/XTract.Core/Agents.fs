f2f83b03a8b3f2cfeaf87ae3f04fb0ea6f793b5d
Taha Hachana <tahahachana@gmail.com>
2015-03-19 19:23:04 +0100

    Clean and comment the agents module

--- Src/XTract.Core/Agents.fs
+++ Src/XTract.Core/Agents.fs
@@ -1,137 +1,136 @@
-﻿[<AutoOpen>]
-module XTract.Agents
+﻿module XTract.Agents
 
+open System
 open System.Collections.Concurrent
 
+// Messages received by the throttling agent
 type Message =
     | Work
-    | WorkAndReply of AsyncReplyChannel<unit>
+    | Start of AsyncReplyChannel<unit>
     | Pause
     | Resume
     | Quit
 
+// Throttling agent status
+type Status =
+    | Idle
+    | Paused
+    | Working
+
+/// <summary>
+/// An agent for throttling the number of concurrently
+/// executing asynchronous workflows. The default limit
+/// is 5 concurrent tasks. The agent also supports pausing
+/// and resuming work.
+/// </summary>
 type ThrottlingAgent(?Limit) as this =
 
-    let limit = defaultArg Limit 5
-    let stack = ConcurrentStack<Async<unit>>()
-    [<DefaultValue>] val mutable private replyChannel : AsyncReplyChannel<unit>
+    let mutable status = Idle
 
-    let dequeue() =
-        match stack.TryPop() with
+    let limit = defaultArg Limit 5
+
+    // Stores the asynchronous workflows waiting for execution.
+    let tasksStack = ConcurrentStack<Async<unit>>()
+
+    // The agent expects to receive a "Start" message first.
+    // The message carries a reply channel that is used to
+    // notify the caller once all the queued asynchronous
+    // workflows are executed.
+    [<DefaultValue>] val mutable private replyChannel: AsyncReplyChannel<unit>
+
+    // Attempts to pop and return an asynchronous workflow
+    // from the tasks stack.
+    let tryPopTask() =
+        match tasksStack.TryPop() with
         | false, _ -> None
         | true, workflow -> Some workflow
 
     let agent =
-        MailboxProcessor.Start(fun x ->
-            let rec loop count doWork =
+        MailboxProcessor.Start(fun inbox ->
+            let rec loop count =
                 async {
-                    let! msg = x.Receive()
+                    let! msg = inbox.Receive()
                     match msg with
-                    | Pause -> return! loop count false
+                    | Pause ->
+                        status <- Paused
+                        let! _ = inbox.Scan(fun msg ->
+                            match msg with
+                            | Resume -> Some (async {do ()})
+                            | _ -> None
+                        )
+                        status <- Working 
+                        return! loop count
                     | Resume ->
-                        [1 .. limit] |> List.iter (fun _ -> x.Post Work)
-                        return! loop count true
-                    | WorkAndReply reply ->
+                        return! loop count
+                    | Start reply ->
+                        status <- Working
                         this.replyChannel <- reply
-                        [1 .. limit] |> List.iter (fun _ -> x.Post Work)
-                        return! loop count true
+                        [1 .. limit] |> List.iter (fun _ -> inbox.Post Work)
+                        return! loop count
                     | Work ->
-                        match doWork with
-                        | false -> return! loop count false
-                        | true ->
-                            let work = dequeue()
-                            match work with
-                            | Some work' ->
-                                async {
-                                    try
-                                        do! work'
-                                    finally
-                                        x.Post Work
-                                } |> Async.Start
-                                return! loop count true
-                            | None ->
-                                x.Post Quit
-                                return! loop (count + 1) true
+                        let workOption = tryPopTask()
+                        match workOption with
+                        | Some work ->
+                            async {
+                                try
+                                    do! work
+                                finally
+                                    inbox.Post Work
+                            } |> Async.Start
+                            return! loop count
+                        | None ->
+                            inbox.Post Quit
+                            return! loop (count + 1)
                     | Quit ->
                         match count with
                         | _ when count = limit ->
                             this.replyChannel.Reply()
-                            return! loop 0 true
-                        | _ -> return! loop count true
+                            status <- Idle
+                            return! loop 0
+                        | _ -> return! loop count
                 }
-            loop 0 true
+            loop 0
         )
 
-    /// Throttles running the specified async workflows.
+    interface IDisposable with
+        member __.Dispose() =
+            agent :> IDisposable
+            |> fun x -> x.Dispose()
+
+    /// <summary>
+    /// Throttles executing the supplied asynchronous workflows.
+    /// </summary>
+    /// <param name="asyncs">The asynchronous workflows.</param>
     member __.Work asyncs =
-        stack.PushRange <| Seq.toArray asyncs
-        agent.PostAndAsyncReply(fun replyChannel -> WorkAndReply replyChannel)
+        async {
+            match status with
+            | Idle ->
+                tasksStack.PushRange <| Seq.toArray asyncs
+                do! agent.PostAndAsyncReply(fun replyChannel -> Start replyChannel)
+            | Working -> failwith "The agent is currently working."
+            | Paused -> failwith "The agent is currently paused."
+        }
 
-    /// Returns the number of remaining async workflows to execute.
-    member __.RemainingTasks = stack.Count
+    /// Gets the count of the asynchronous workflows waiting
+    // for execution.
+    member __.RemainingTasks = tasksStack.Count
 
-    /// Pauses the throttler.
-    member __.Pause() = agent.Post Pause
+    /// Pauses the agent.
+    member __.Pause() =
+        match status with
+        | Working -> agent.Post Pause
+        | Paused -> failwith "The agent has already been paused."
+        | Idle -> failwith "The agent is idle and can't be paused."
 
-    /// Resumes running the async workflows.
-    member __.Resume() = agent.Post Resume
+    /// Resumes executing the asynchronous workflows.
+    member __.Resume() =
+        match status with
+        | Paused -> agent.Post Resume
+        | Working -> failwith "The agent is already working."
+        | Idle -> failwith "The agent is idle, there is no work to resume."
 
-    /// Cancels running the remaining async workflows.
-    member __.CancelRemainingTasks() = stack.Clear()
+    /// Cancels executing the remaining asynchronous workflows.
+    member __.CancelRemainingTasks() = tasksStack.Clear()
 
-//let asyncs =
-//    [1 .. 10]
-//    |> List.map (fun x ->
-//        async {
-//            do! Async.Sleep (x * 1000)
-//            printfn "%d" x
-//        })
-//
-//let test = ThrottlingAgent()
-//
-//test.Work asyncs
-//|> Async.Start
-//
-//test.RemainingTasks
-//
-//test.Pause()
-//
-//test.Resume()
-
-
-//let throttle (asyncs:seq<Async<unit>>) limit callback =
-// 
-//    let q = Queue(asyncs)
-//    
-//    let dequeue() = try q.Dequeue() |> Some with _ -> None
-//    
-//    let agent =
-//        MailboxProcessor.Start(fun x ->
-//            let rec loop count =
-//                async {
-//                    let! msg = x.Receive()
-//                    match msg with
-//                    | Work ->
-//                        let work = dequeue()
-//                        match work with
-//                        | Some work' ->
-//                            async {
-//                                try
-//                                    do! work'
-//                                finally
-//                                    x.Post Work
-//                            } |> Async.Start
-//                            return! loop count
-//                        | None ->
-//                            x.Post Quit
-//                            return! loop (count + 1)
-//                    | Quit ->
-//                        match count with
-//                        | _ when count = limit ->
-//                            callback |> Async.Start
-//                            (x:> IDisposable).Dispose()
-//                        | _ -> return! loop count
-//                }
-//            loop 0
-//        )
-//    [1 .. limit] |> List.iter (fun _ -> agent.Post Work)
+    /// Gets the agent's status.
+    member __.Status = status
